730d7de4caa6cb33681ffdbcae00a586ca0cf3ca
ahmed <ahmed@double-eye.com>
2017-04-21 11:32:30 +0200

    [changes] completed step 1

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.JokeTell;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        JokeTell jokeTell = new JokeTell();
+        Toast.makeText(this, jokeTell.getRandomJoke(), Toast.LENGTH_LONG).show();
     }
 
 
--- jokeTellLib/src/main/java/com/example/JokeTell.java
+++ jokeTellLib/src/main/java/com/example/JokeTell.java
@@ -1,4 +1,28 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class JokeTell {
+
+    private List<String> jokes = new ArrayList<>();
+    private String joke;
+
+    public String getRandomJoke() {
+        generateJokeList();
+
+        if (jokes.size() > 0) {
+            Random random = new Random();
+            joke = jokes.get(random.nextInt(jokes.size()));
+        }
+        return joke;
+    }
+
+    private void generateJokeList() {
+        jokes.add("Why does a chicken coop have two doors? Because if it had four doors it would be a chicken sedan.");
+        jokes.add("How do you find Will Smith in the snow? Look for the fresh prints.");
+        jokes.add("What do you get when you cross the Atlantic with the Titanic?About half way.");
+    }
+
 }
