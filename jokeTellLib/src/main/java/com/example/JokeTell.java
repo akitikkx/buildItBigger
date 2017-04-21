6cd9c89873cbf86aba2c0f492666c8771aea14b3
ahmed <ahmed@double-eye.com>
2017-04-21 11:40:48 +0200

    [changes] completed step 1

--- jokeTellLib/src/main/java/com/example/JokeTell.java
+++ jokeTellLib/src/main/java/com/example/JokeTell.java
@@ -22,7 +22,11 @@ public class JokeTell {
     private void generateJokeList() {
         jokes.add("Why does a chicken coop have two doors? Because if it had four doors it would be a chicken sedan.");
         jokes.add("How do you find Will Smith in the snow? Look for the fresh prints.");
-        jokes.add("What do you get when you cross the Atlantic with the Titanic?About half way.");
+        jokes.add("What do you get when you cross the Atlantic with the Titanic? About half way.");
+        jokes.add("Have you heard of the new restaurant on the moon? The food is amazing, but I've heard its got no atmosphere...");
+        jokes.add("What kind of shoes are made from bananas skins? Slippers.");
+        jokes.add("What kind of rooms have no walls? Mushrooms.");
+        jokes.add("What happened to the boy who drank 8 cokes? He burped 7-Up.");
     }
 
 }
